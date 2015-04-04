96c8da72029527d15b9d5b220d3a586a1ace5073
flyboy14 <say.bzzzzz@gmail.com>
2015-04-04 03:42:06 +0300

    fixed translation script

--- scripts/translate_ru_en.sh
+++ scripts/translate_ru_en.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #notify-send -i $HOME/.config/awesome/icons/comicdee/translate.svg "$(xsel -o)" "$(wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$(xsel -o | sed "s/[\"'<>]//g")&sl=auto&tl=ru" | sed 's/\[\[\[\"//' | cut -d \" -f 1)"
 INPUT=$(xsel -o)
-OUTPUT=$(wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$(xsel -o | sed "s/[\"'<>]//g")&sl=auto&tl=ru" | sed 's/\[\[\[\"//' | cut -d \" -f 1)
+OUTPUT=$(wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$(xsel -o | sed "s/[\"'<>]//g")&sl=ru&tl=en" | sed 's/\[\[\[\"//' | cut -d \" -f 1)
 echo 'show_smth("'$INPUT'", "'$OUTPUT'", "'$HOME/.config/awesome/icons/comicdee/translate.svg'", 3.5)' | awesome-client
