fe9571b9090a7e1cb3e6eefb4d21975f6dc42134
flyboy14 <say.bzzzzz@gmail.com>
2015-05-17 23:06:44 +0300

    minor fixes

--- vicious/widgets/gmoil.lua
+++ vicious/widgets/gmoil.lua
@@ -49,7 +49,8 @@ local mail = {
 -- {{{ Gmail widget type
 local function worker(format, warg)
     -- Get info from the Gmail atom feed
-    local f = io.popen("curl --connect-timeout 1 -m 3 --fail --silent -u yaour_email@example.com:your_password " .. feed[1])
+    local f = io.popen("curl --connect-timeout 1 -m 3 --fail --silent -u 
+your_email@example.smth:your_password " .. feed[1])
 
     -- Could be huge don't read it all at once, info we are after is at the top
     for line in f:lines() do
