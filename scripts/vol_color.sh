31e80a6cda5d73b89ef3ecdb15b9fbcc757f8338
flyboy14 <say.bzzzzz@gmail.com>
2015-05-17 20:05:33 +0300

    minor fixes

--- scripts/vol_color.sh
+++ scripts/vol_color.sh
@@ -30,41 +30,41 @@ STATE=$(amixer -c 0 get Master | grep 'Mono:' | grep -o "\[off\]")
 if [[ "$STATE" != "" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_muted_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
 else
-if [ $VOLUME == "0" ]; then
+if [[ $VOLUME == "0" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_0_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client  
-elif [ $VOLUME == "100" ]; then
+elif [[ $VOLUME == "100" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_100_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 9 ]; then
+elif [[ $VOLUME -lt 9 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_5_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 13 ]; then
+elif [[ $VOLUME -lt 13 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_10_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 19 ]; then
+elif [[ $VOLUME -lt 19 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_15_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 26 ]; then
+elif [[ $VOLUME -lt 26 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_21_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 32 ]; then
+elif [[ $VOLUME -lt 32 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_27_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 38 ]; then
+elif [[ $VOLUME -lt 38 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_32_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 40 ]; then
+elif [[ $VOLUME -lt 40 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_38_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 45 ]; then
+elif [[ $VOLUME -lt 45 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_44_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 52 ]; then
+elif [[ $VOLUME -lt 52 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_51_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client    
-elif [ $VOLUME -lt 56 ]; then
+elif [[ $VOLUME -lt 56 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_56_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 62 ]; then
+elif [[ $VOLUME -lt 62 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_62_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 71 ]; then
+elif [[ $VOLUME -lt 71 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_68_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 78 ]; then
+elif [[ $VOLUME -lt 78 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_75_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 86 ]; then
+elif [[ $VOLUME -lt 86 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_83_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 91 ]; then
+elif [[ $VOLUME -lt 91 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_88_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 96 ]; then
+elif [[ $VOLUME -lt 96 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/color_arrows/icons/vol_94_x16.png'", 2, "#E2AE7C", nil, nil, nil)' | awesome-client 
 fi
 fi
--- scripts/vol_pa_dark.sh
+++ scripts/vol_pa_dark.sh
@@ -29,41 +29,41 @@ STATE=$($SETVOL --get-mute)
 if [[ "$STATE" == "true" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_muted_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
 else
-if [ $VOLUME == "0" ]; then
+if [[ $VOLUME == "0" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_0_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client  
-elif [ $VOLUME == "100" ]; then
+elif [[ $VOLUME == "100" ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_100_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 9 ]; then
+elif [[ $VOLUME -lt 9 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_5_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 13 ]; then
+elif [[ $VOLUME -lt 13 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_10_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 19 ]; then
+elif [[ $VOLUME -lt 19 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_15_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 26 ]; then
+elif [[ $VOLUME -lt 26 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_21_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 32 ]; then
+elif [[ $VOLUME -lt 32 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_27_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 38 ]; then
+elif [[ $VOLUME -lt 38 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_32_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 40 ]; then
+elif [[ $VOLUME -lt 40 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_38_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 45 ]; then
+elif [[ $VOLUME -lt 45 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_44_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 52 ]; then
+elif [[ $VOLUME -lt 52 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_51_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client    
-elif [ $VOLUME -lt 56 ]; then
+elif [[ $VOLUME -lt 56 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_56_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 62 ]; then
+elif [[ $VOLUME -lt 62 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_62_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 71 ]; then
+elif [[ $VOLUME -lt 71 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_68_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 78 ]; then
+elif [[ $VOLUME -lt 78 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_75_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 86 ]; then
+elif [[ $VOLUME -lt 86 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_83_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 91 ]; then
+elif [[ $VOLUME -lt 91 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_88_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
-elif [ $VOLUME -lt 96 ]; then
+elif [[ $VOLUME -lt 96 ]]; then
   echo 'show_smth(nil, nil, "'$HOME/.config/awesome/themes/dark_grey/icons/vol_94_x16.png'", 2, "#121212", nil, nil, nil)' | awesome-client 
 fi
 fi
